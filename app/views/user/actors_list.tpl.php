7dcbf7b2ac22df3b9212b982460a5830009a0c39
githoweb <githoweb@gmail.com>
2024-04-04 17:01:07 +0200

    save

--- app/views/user/actors_list.tpl.php
+++ app/views/user/actors_list.tpl.php
@@ -1,36 +1,20 @@
 <div>
     <a href="<?= $router->generate('actor-add') ?>" class="btn btn-success">Ajouter</a>
     <h2>Liste des Acteurs</h2>
-    <table>
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Prénom</th>
-                <th scope="col">Nom</th>
-                <th scope="col">Photo</th>
-                <th scope="col">Naissance</th>
-                <th scope="col">Biographie</th>
-                <th>-</th>
-            </tr>
-        </thead>
-        <tbody>
 
-          <?php foreach($actors as $actor) : ?>
+    <div class="personsList">
 
-            <tr>
-                <th scope="row"><?= $actor->getId() ?></th>
-                <td><?= $actor->getFirstName() ?></td>
-                <td><?= $actor->getLastName() ?></td>
-                <td><img src="https://media.themoviedb.org/t/p/w300_and_h450_bestv2/<?= $actor->getPoster() ?>" width=100 /></td>
-                <td><?= $actor->getBirth() ?></td>
-                <td><?= $actor->getBiography() ?></td>
-                <td>
-                    <a href="<?= $router->generate('actor-delete', ['id' => $actor->getId()]) ?>?tokenCsrf=<?= $tokenCsrf ?>">supprimer</a>
-                </td>
-            </tr>
+        <?php foreach($actors as $actor) : ?>
 
-          <?php endforeach ?>
+        <figure>
+            <img src="https://media.themoviedb.org/t/p/w300_and_h450_bestv2/<?= $actor->getPoster() ?>" alt="" />
+            <p><?= $actor->getFirstName() ?> <?= $actor->getLastName() ?></p>
+            #<?= $actor->getId() ?>
+            <a href="<?= $router->generate('actor-delete', ['id' => $actor->getId()]) ?>?tokenCsrf=<?= $tokenCsrf ?>">supprimer</a>
+        </figure>
 
-        </tbody>
-    </table>
+        <?php endforeach ?>
+
+    </div>
+
 </div>
--- app/views/user/directors_list.tpl.php
+++ app/views/user/directors_list.tpl.php
@@ -1,36 +1,19 @@
 <div>
     <a href="<?= $router->generate('director-add') ?>" class="btn btn-success">Ajouter</a>
     <h2>Liste des Réalisateurs</h2>
-    <table>
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Prénom</th>
-                <th scope="col">Nom</th>
-                <th scope="col">Photo</th>
-                <th scope="col">Naissance</th>
-                <th scope="col">Biographie</th>
-                <th>-</th>
-            </tr>
-        </thead>
-        <tbody>
 
-          <?php foreach($directors as $director) : ?>
+    <div class="personsList">
 
-            <tr>
-                <th scope="row"><?= $director->getId() ?></th>
-                <td><?= $director->getFirstName() ?></td>
-                <td><?= $director->getLastName() ?></td>
-                <td><img src="https://media.themoviedb.org/t/p/w300_and_h450_bestv2/<?= $director->getPoster() ?>" width=100 /></td>
-                <td><?= $director->getBirth() ?></td>
-                <td><?= $director->getBiography() ?></td>
-                <td>
-                    <a href="<?= $router->generate('director-delete', ['id' => $director->getId()]) ?>?tokenCsrf=<?= $tokenCsrf ?>">supprimer</a>
-                </td>
-            </tr>
+        <?php foreach($directors as $director) : ?>
 
-          <?php endforeach ?>
+        <figure>
+            <img src="https://media.themoviedb.org/t/p/w300_and_h450_bestv2/<?= $director->getPoster() ?>" alt="" />
+            <p><?= $director->getFirstName() ?> <?= $director->getLastName() ?></p>
+            #<?= $director->getId() ?>
+            <a href="<?= $router->generate('actor-delete', ['id' => $director->getId()]) ?>?tokenCsrf=<?= $tokenCsrf ?>">supprimer</a>
+        </figure>
 
-        </tbody>
-    </table>
+        <?php endforeach ?>
+
+    </div>
 </div>
